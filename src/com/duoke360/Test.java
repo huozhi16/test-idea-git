7d84f01e1b0e25c1834dc9b0d11c5a383df38847
wlz <565237049@qq.com>
2023-06-27 15:46:44 +0800

    v3

--- src/com/duoke360/Test.java
+++ src/com/duoke360/Test.java
@@ -4,5 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello java v1");
         System.out.println("hello java v2");
+        System.out.println("hello java v3");
+
     }
 }
